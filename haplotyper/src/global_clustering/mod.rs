4d5eb42069c18eb349e4d49bbd306d69665861a0
ban-m <ban-m@g.ecc.u-tokyo.ac.jp>
2020-07-30 15:08:53 +0900

    split de Bruijn graph into other crate

--- haplotyper/src/global_clustering/mod.rs
+++ haplotyper/src/global_clustering/mod.rs
@@ -1,13 +1,73 @@
-use super::find_union::FindUnion;
+use de_bruijn_graph::*;
 use std::collections::HashMap;
 use std::collections::HashSet;
-mod de_bruijn_graph;
 mod error_correction;
-use de_bruijn_graph::*;
 // use error_correction::local_correction;
 use error_correction::CorrectedRead;
 pub mod path_clustering;
 pub use path_clustering::path_clustering;
+
+struct ReadWrapper<'a>(&'a definitions::EncodedRead);
+
+impl<'a> ReadWrapper<'a> {
+    fn new(x: &'a definitions::EncodedRead) -> Self {
+        Self(x)
+    }
+    fn id(&self) -> u64 {
+        self.0.id
+    }
+}
+
+impl<'a> IntoDeBruijnNodes for ReadWrapper<'a> {
+    fn into_de_bruijn_nodes(&self, k: usize) -> Vec<Node> {
+        self.0
+            .nodes
+            .windows(k)
+            .map(|w| {
+                let first = {
+                    let f = w.first().unwrap();
+                    (f.unit, f.cluster)
+                };
+                let last = {
+                    let l = w.last().unwrap();
+                    (l.unit, l.cluster)
+                };
+                let kmer: Vec<_> = if first < last {
+                    w.iter().map(|n| (n.unit, n.cluster)).collect()
+                } else {
+                    w.iter().rev().map(|n| (n.unit, n.cluster)).collect()
+                };
+                Node::new(kmer)
+            })
+            .collect()
+    }
+}
+
+impl IntoDeBruijnNodes for CorrectedRead {
+    fn into_de_bruijn_nodes(&self, k: usize) -> Vec<Node> {
+        self.nodes.windows(k).map(AsDeBruijnNode::as_node).collect()
+    }
+}
+
+impl AsDeBruijnNode for error_correction::Unit {
+    fn as_node(w: &[error_correction::Unit]) -> Node {
+        let first = {
+            let f = w.first().unwrap();
+            (f.unit, f.cluster)
+        };
+        let last = {
+            let l = w.last().unwrap();
+            (l.unit, l.cluster)
+        };
+        let kmer: Vec<_> = if first < last {
+            w.iter().map(|n| (n.unit, n.cluster)).collect()
+        } else {
+            w.iter().rev().map(|n| (n.unit, n.cluster)).collect()
+        };
+        Node::new(kmer)
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct GlobalClusteringConfig {
     pub k_mer: usize,
@@ -46,16 +106,8 @@ impl GlobalClustering for definitions::DataSet {
             let tot = length.iter().sum::<usize>();
             eprintln!("Read({}){}\n{}", length.len(), tot, hist.format(20, 40));
         }
-        // let reads = local_correction(&self, c);
-        // debug!("Corrected reads.");
-        // if log_enabled!(log::Level::Debug) {
-        //     let length: Vec<_> = reads.iter().map(|r| r.nodes.len()).collect();
-        //     let hist = histgram_viz::Histgram::new(&length);
-        //     let tot = length.iter().sum::<usize>();
-        //     eprintln!("Read({}){}\n{}", length.len(), tot, hist.format(20, 40));
-        // }
-        //let graph = DeBruijnGraph::from_corrected_reads(&reads, c.k_mer);
-        let graph = DeBruijnGraph::from_encoded_reads(&self.encoded_reads, c.k_mer);
+        let reads: Vec<_> = self.encoded_reads.iter().map(ReadWrapper::new).collect();
+        let graph = DeBruijnGraph::from(&reads, c.k_mer);
         if log_enabled!(log::Level::Debug) {
             let mut count: Vec<_> = graph.nodes.iter().map(|n| n.occ).collect();
             count.sort();
@@ -82,16 +134,15 @@ impl GlobalClustering for definitions::DataSet {
             count.sort_by_key(|x| x.0);
             eprintln!("Degree Count\n{:?}", count);
         }
-        graph.coloring(c);
+        graph.coloring();
         let component_num = graph.nodes.iter().map(|n| n.cluster).max().unwrap() + 1;
         debug!("Resulting in {} clusters.", component_num);
         let mut count: HashMap<_, usize> = HashMap::new();
-        let assignments: Vec<_> = self
-            .encoded_reads
+        let assignments: Vec<_> = reads
             .iter()
             .filter_map(|read| {
-                let id = read.id;
-                graph.assign_encoded_read(&read).map(|cluster| {
+                let id = read.id();
+                graph.assign_read(read).map(|cluster| {
                     *count.entry(cluster).or_default() += 1;
                     definitions::Assignment { id, cluster }
                 })
