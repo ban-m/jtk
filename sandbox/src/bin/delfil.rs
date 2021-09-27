ef8f8cdcd8a500388c57760b71d8366ba598dd3f
Masutani BANSHO <ban-m@g.ecc.u-tokyo.ac.jp>
2021-09-27 17:37:15 +0900

    update

--- sandbox/src/bin/delfil.rs
+++ sandbox/src/bin/delfil.rs
@@ -8,26 +8,27 @@ fn main() -> std::io::Result<()> {
     let mut ds: DataSet = std::fs::File::open(&args[1])
         .map(BufReader::new)
         .map(|x| serde_json::de::from_reader(x).unwrap())?;
-    let target = 520;
-    let mut neighbor: HashSet<_> = HashSet::new();
-    for edge in ds.encoded_reads.iter().flat_map(|r| r.edges.iter()) {
-        if edge.from == target {
-            neighbor.insert(edge.to);
-        }
-        if edge.to == target {
-            neighbor.insert(edge.from);
-        }
-    }
-    let target_reads: HashSet<_> = ds
-        .encoded_reads
-        .iter()
-        .filter_map(|r| {
-            r.nodes
-                .iter()
-                .any(|n| neighbor.contains(&n.unit))
-                .then(|| r.id)
-        })
-        .collect();
-    haplotyper::encode::deletion_fill::correct_unit_deletion_selected(&mut ds, &target_reads, 0.35);
+    // let target = 520;
+    // let mut neighbor: HashSet<_> = HashSet::new();
+    // for edge in ds.encoded_reads.iter().flat_map(|r| r.edges.iter()) {
+    //     if edge.from == target {
+    //         neighbor.insert(edge.to);
+    //     }
+    //     if edge.to == target {
+    //         neighbor.insert(edge.from);
+    //     }
+    // }
+    // let target_reads: HashSet<_> = ds
+    //     .encoded_reads
+    //     .iter()
+    //     .filter_map(|r| {
+    //         r.nodes
+    //             .iter()
+    //             .any(|n| neighbor.contains(&n.unit))
+    //             .then(|| r.id)
+    //     })
+    //     .collect();
+    let mut failed = vec![vec![]; ds.encoded_reads.len()];
+    ds = haplotyper::encode::deletion_fill::correct_unit_deletion(ds, &mut failed, 0.35);
     Ok(())
 }
